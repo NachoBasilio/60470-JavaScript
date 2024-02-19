2866cf650802c70fd50048395fe149d1ce79dcbe
IgnaDev <arcortoon@gmail.com>
2024-02-19 11:09:01 -0300

    first commit

---
+++ Semana 1/ejercicios.js
@@ -0,0 +1,42 @@
+// Variables:
+
+/*
+
+1. Inicializar tres variables de distintos tipos de datos.
+
+2. Declarar dos variables y asignarles valor más adelante.
+
+3. Mostrar las 5 variables anteriores por consola con el operador typeof para verificar los tipos de datos.
+
+4. Ejemplo de variable con la declaración "const".
+
+*/
+
+
+// -----------------------------------------------
+
+// Operaciones básicas
+
+/*
+
+1. Utilizar dos de las cuatro operaciones básicas asignando el valor en una variable y mostrarla por consola.
+
+2. Utilizar las otras dos operaciones restantes directamente por consola sin utilizar una variable.
+
+
+*/
+
+
+// -----------------------------------------------
+
+// Funciones nativas / Concatenación
+
+/*
+
+1. Ingresar nombre y apellido del usuario por prompt y consultar si los datos son correctos a través de un confirm.
+
+2. Repetir el ejercicio de operaciones básicas pero ingresando los valores numéricos a través de prompt y mostrando un mensaje con el resultado por alert.
+
+3. Repetir el ejercicio de declarar una variable y asignarle el valor luego, pero a través de prompt. Mostrar el resultado por consola y por alert.
+
+*/
