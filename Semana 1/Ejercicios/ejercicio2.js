b730d2ae708c20bdbb097372da0d6fc9c3007fb8
IgnaDev <arcortoon@gmail.com>
2024-02-19 22:27:14 -0300

    Semana 1

--- Semana 1/Ejercicios/ejercicio2.js
+++ Semana 1/Ejercicios/ejercicio2.js
@@ -6,5 +6,22 @@
 
 2. Utilizar las otras dos operaciones restantes directamente por consola sin utilizar una variable.
 
-
++ - * /
 */
+
+let numero1 = Number(prompt("Numero 1"))
+let numero2 = Number(prompt("Numero 2"))
+
+let suma = numero1 + numero2 //?
+
+let resta = numero1 - numero2 //?
+
+let multi = numero1 * numero2 //?
+
+let div = numero1 / numero2 //?
+
+let mod = numero1 % numero2 //?
+
+
+alert("Los resultados son los siguientes: " + suma + ", " + resta + ", " + multi + ", " + div.toFixed(2) + ", " + mod)
+
