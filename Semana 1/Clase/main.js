a0be768798fa5c2442f38adbe1078e448cea4c20
IgnaDev <arcortoon@gmail.com>
2024-02-19 22:14:05 -0300

    Semana 1

---
+++ Semana 1/Clase/main.js
@@ -0,0 +1,36 @@
+
+//let = variable
+
+let hola //undefined
+console.log(hola)//undefined
+hola = "hola"
+console.log(hola)//"hola"
+
+let nombre = "Pep"
+
+const DNI = 12312311
+
+let vivo = true 
+
+
+
+console.log(nombre)
+
+nombre = "Pepe"
+
+console.log(nombre)
+
+console.log(typeof vivo)
+
+nombre = 25
+
+console.log(nombre)
+
+console.log(DNI / 2)
+
+let edad = prompt("¿Cuantos años tenes?")
+
+edad = parseFloat(edad)
+
+alert(edad + 2)
+
